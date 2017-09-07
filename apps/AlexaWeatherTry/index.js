3440f886dbc517ec0500107cfe81af23fe9faee9
38136 <38136.hexaware@gmail.com>
2017-09-07 15:23:15 +0530

    Add All Files

--- apps/AlexaWeatherTry/index.js
+++ apps/AlexaWeatherTry/index.js
@@ -6,7 +6,7 @@ var app = new alexa.app('test-skill');
 
 
 app.launch(function (request, response) {
-    response.say('Welcome to Weather Forecasting').reprompt('You want to know about the today forecast').shouldEndSession(false);
+    response.say('Welcome to Weather Forecasting').reprompt('You want to know about the today forecast').shouldEndSession(false).remprompt('I\'m still listening.');
 });
 
 
@@ -17,16 +17,23 @@ app.error = function (exception, request, response) {
     response.say('Sorry an error occured ' + error.message);
 };
 app.intent('WelcomeIntent', function (request, response) {
-    response.say("Welcome to Weather Forecasting do you want to know about today's Monsoon").shouldEndSession(false);
+    response.say("Welcome to Weather Forecasting do you want to know about today's Monsoon").shouldEndSession(false).remprompt('I\'m still listening.');
 });
 app.intent('WeatherIntent',
     function (request, response) {
         var city = request.slot('cityname');
+        var weatherreport=require('./weather')(city);
+        let desc = weather.description;
+        let humidity = main.humidity;
         if (city) {
-            response.say("Today weather looks great in " + city + "with 33");
-        }else {
-            response.say("please tell me your city name").shouldEndSession(false);
+            response.say("Today weather looks " + description + " in " + city + "with humidity is " + humidity+"Do you like to continue.").shouldEndSession(false).remprompt('I\'m still listening.');
+        } else {
+            response.say("please tell me your city name").shouldEndSession(false).remprompt('I\'m still listening.');
         }
     });
+
+app.intent('ThankYouIntent', function (request, response) {
+    response.say("Thank you, Namdri and dhanniyavaath");
+});
 
 module.exports = app;
